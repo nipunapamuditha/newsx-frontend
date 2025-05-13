efbd3209cb984aff260f9b42b529efedbe1a8f1e
nipunapamuditha <nipunapamuditha234@gmail.com>
2025-05-13 11:01:12 -0400

    personalize

--- src/Login.tsx
+++ src/Login.tsx
@@ -91,7 +91,7 @@ const Login = () => {
             <h1 style={styles.appTitle}>
               NEW<span style={styles.xHighlight}>X</span>
             </h1>
-            <p style={styles.loginSubtitle}>Sign in to continue</p>
+            <p style={styles.loginSubtitle}>Sign in to continue . Name - 500238003 Nipuna Karunarathna</p>
             <div style={styles.googleLoginWrapper}>
               <GoogleLogin
                 onSuccess={handleLoginSuccess}
